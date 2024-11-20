ce986368290f394d6d476c8a75d475b021c9687b
Fast and Static <it@e-me.co>
2024-11-20 20:53:50 +0000

    update

--- wp-content/plugins/pro-elements/assets/js/jszip.vendor.a3c65615c1de5560962d.bundle.js
+++ wp-content/plugins/pro-elements/assets/js/jszip.vendor.a3c65615c1de5560962d.bundle.js
@@ -1,4 +1,4 @@
-/*! pro-elements - v3.23.0 - 05-08-2024 */
+/*! pro-elements - v3.25.0 - 13-11-2024 */
 (self["webpackChunkelementor_pro"] = self["webpackChunkelementor_pro"] || []).push([["jszip.vendor"],{
 
 /***/ "../node_modules/jszip/dist/jszip.min.js":
